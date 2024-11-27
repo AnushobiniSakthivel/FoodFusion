f11f1d4f6e2566671d3186737e5784a731469535
ABDUL HAJEES <84639525+aj05hacker@users.noreply.github.com>
2024-11-27 14:58:50 +0530

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,5 @@
+// Toggle the menu for small screens
+document.getElementById("menu-btn").addEventListener("click", function() {
+    const menu = document.querySelector('nav ul');
+    menu.classList.toggle('show');
+});
